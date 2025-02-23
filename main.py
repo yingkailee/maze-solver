ce7b696ab5b9fbe512bfe536415d282d8afca93d
yingkailee <yingkaileee@gmail.com>
2025-02-22 22:56:59 -0600

    added cell logic

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from modules import Window, Point, Line
+from modules import Window, Point, Line, Cell
 
 def main():
     window = Window(800,800)
-    pt1, pt2 = Point(5, 5), Point(100, 100)
-    line = Line(pt1, pt2)
-    window.draw_line(line, "red")
+    #window.draw_line(Line(Point(5, 5), Point(100, 100)), "red")
+    full_cell = Cell(window,5,5,100,100)
+    full_cell.draw()
+    vert_cell = Cell(window,500,500,600,600,True, True, False, False)
+    vert_cell.draw()
+    hori_cell = Cell(window,100,600,200,700,False, False)
+    hori_cell.draw()
+    vert_cell.draw_move(hori_cell)
     window.wait_for_close()
 
 if __name__ == "__main__":
--- modules.py
+++ modules.py
@@ -37,3 +37,37 @@ class Line:
     
     def draw(self, canvas, fill_color):
         canvas.create_line(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self, window, x1, y1, x2, y2, has_left=True, has_right=True, has_top=True, has_bot=True):
+        self.window = window
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.has_left = has_left
+        self.has_right = has_right
+        self.has_top = has_top
+        self.has_bot = has_bot
+    
+    # (x1,y1) is top left
+    # (x2,y2) is bottom right
+    def draw(self):
+        top_left = Point(self.x1, self.y1)
+        bottom_left = Point(self.x1, self.y2)
+        top_right = Point(self.x2, self.y1)
+        bottom_right = Point(self.x2, self.y2)
+        if (self.has_left):
+            self.window.draw_line(Line(top_left, bottom_left), "black")
+        if (self.has_right):
+            self.window.draw_line(Line(top_right, bottom_right), "black")
+        if (self.has_top):
+            self.window.draw_line(Line(top_left, top_right), "black")
+        if (self.has_bot):
+            self.window.draw_line(Line(bottom_left, bottom_right), "black")
+    
+    def draw_move(self, to_cell, undo=False):
+        fill_color = 'gray' if undo else 'red'
+        from_center = Point((self.x1+self.x2)//2, (self.y1+self.y2)//2)
+        to_center = Point((to_cell.x1+to_cell.x2)//2, (to_cell.y1+to_cell.y2)//2)
+        self.window.draw_line(Line(from_center, to_center), fill_color)
